f868cf8bcb394604f951b9f093ef13743fdf5f14
Hristo Stefanov <hristo.m.stefanov@gmail.com>
2021-09-03 19:32:42 +0100

    Implement MainViewModel so as the test passes

--- app/src/main/java/hristostefanov/creditscoredemo/presentation/MainViewModel.kt
+++ app/src/main/java/hristostefanov/creditscoredemo/presentation/MainViewModel.kt
@@ -1,10 +1,12 @@
 package hristostefanov.creditscoredemo.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hristostefanov.creditscoredemo.business.dependencies.CreditScoreRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -12,4 +14,14 @@ class MainViewModel @Inject constructor(creditScoreRepository: CreditScoreReposi
     ViewModel() {
     private val _viewState = MutableStateFlow(MainViewState())
     val viewState: StateFlow<MainViewState> = _viewState
+
+    init {
+        viewModelScope.launch {
+            _viewState.value = creditScoreRepository.findCreditScore().let {
+                val scoreText = it.score.toString()
+                val caption = "out of ${it.maxScore}"
+                MainViewState(scoreText = scoreText, caption = caption)
+            }
+        }
+    }
 }

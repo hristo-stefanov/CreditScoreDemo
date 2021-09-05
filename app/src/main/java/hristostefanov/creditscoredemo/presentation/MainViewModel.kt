848a553fbe85c4d8ead98c918f15af60718dd814
Hristo Stefanov <hristo.m.stefanov@gmail.com>
2021-09-05 19:44:08 +0100

    Improve formatting of MainViewModel code

--- app/src/main/java/hristostefanov/creditscoredemo/presentation/MainViewModel.kt
+++ app/src/main/java/hristostefanov/creditscoredemo/presentation/MainViewModel.kt
@@ -4,9 +4,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import hristostefanov.creditscoredemo.R
-import hristostefanov.creditscoredemo.util.StringSupplier
 import hristostefanov.creditscoredemo.business.DataAccessException
 import hristostefanov.creditscoredemo.business.ReportCreditScoreProgressInteractor
+import hristostefanov.creditscoredemo.util.StringSupplier
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -16,8 +16,8 @@ import javax.inject.Inject
 class MainViewModel @Inject constructor(
     private val reportCreditScoreProgressInteractor: ReportCreditScoreProgressInteractor,
     private val stringSupplier: StringSupplier
-) :
-    ViewModel() {
+) : ViewModel() {
+
     private val _viewState = MutableStateFlow<MainViewState>(MainViewState.Loading)
     val viewState: StateFlow<MainViewState> = _viewState
 
